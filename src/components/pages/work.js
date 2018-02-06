5ac693c1202d636502d5fa0437ac879a960ff0e6
Pawel <pawel.klasa@gmail.com>
2018-02-06 23:42:08 +0000

    working on content pages

--- src/components/pages/work.js
+++ src/components/pages/work.js
@@ -4,12 +4,17 @@ import {
     Link
 } from 'react-router-dom';
 
+import './styles/work.css';
+
 export class Work extends React.Component {
     render() {
         return (
             <div class="">
-                <div>
+                <div class="talktalk">
                     Some content
+               </div>
+               <div class="sky">
+                    Some content two
                </div>
             </div>
         )
