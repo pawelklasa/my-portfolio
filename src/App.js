695002b80d17546cddd42229d02794b7ed835c21
Pawel Klasa <pawel.klasa@gmail.com>
2018-02-06 14:30:51 +0000

    created Header component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import React, { Component } from 'react';
 import './styles/global.css';
 
 import { Footer } from './components/footer.js';
+import { Header } from './components/header.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div class="container">
-            <div class="header">
-                <div class="logo">pav<span class="highlight">_</span>ka</div>
-            </div>
-
-            <div class="search">
-                <div class="search-logo">
-                    <span>portfolio | articles | contact</span>
-                </div>
-            </div>
-
+            <Header />
             <div class="content">
                 <h2>I am a <span class="highlight">Web Developer</span> with passion for <span class="highlight">UI</span> and <span class="highlight">UX</span> design. I work with global brands and startups to take new digital products and services to market.</h2>
             </div>
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Header extends React.Component {
+    render() {
+        return (
+            <div class="header">
+                <div class="logo">pav<span class="highlight">_</span>ka</div>
+            </div>
+
+        )
+    }
+}
