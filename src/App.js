c27e40c29b816c31eec5247c60d51478932504d3
Pawel Klasa <pawel.klasa@gmail.com>
2018-02-06 15:33:15 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
+
 import './styles/global.css';
 
 import { Footer } from './components/footer.js';
 import { Header } from './components/header.js';
 import { Content } from './components/content.js';
+import { Nav } from './components/nav.js';
 
 class App extends Component {
   render() {
@@ -12,7 +14,7 @@ class App extends Component {
       <div className="App">
         <div class="container">
             <Header />
-            <div class="nav">Work | Medium | Contact</div>
+            <Nav />
             <Content />
             <Footer />
         </div>
