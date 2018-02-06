fa691e7cd4759d096931b66635ee5ca01ad54662
Pawel Klasa <pawel.klasa@gmail.com>
2018-02-06 14:40:53 +0000

    created Content component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/global.css';
 
 import { Footer } from './components/footer.js';
 import { Header } from './components/header.js';
+import { Content } from './components/content.js';
 
 class App extends Component {
   render() {
@@ -11,9 +12,7 @@ class App extends Component {
       <div className="App">
         <div class="container">
             <Header />
-            <div class="content">
-                <h2>I am a <span class="highlight">Web Developer</span> with passion for <span class="highlight">UI</span> and <span class="highlight">UX</span> design. I work with global brands and startups to take new digital products and services to market.</h2>
-            </div>
+            <Content />
             <Footer />
         </div>
       </div>
---
+++ src/components/content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Content extends React.Component {
+    render() {
+        return (
+            <div class="content">
+                <h2>I am a <span class="highlight">Web Developer</span> with passion for <span class="highlight">UI</span> and <span class="highlight">UX</span> design. I work with global brands and startups to take new digital products and services to market.</h2>
+            </div>
+
+        )
+    }
+}
