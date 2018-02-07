8bd23877b179710d85162784ce1268e492f2f632
Pawel Klasa <pawel.klasa@gmail.com>
2018-02-07 15:36:37 +0000

    work examples page finished

--- src/components/pages/work.js
+++ src/components/pages/work.js
@@ -30,6 +30,16 @@ export class Work extends React.Component {
                     <h3>British Transport Police</h3>
                     <p>Some text description goes here</p>
                 </div>
+                <div class="jlr">
+                    <img src="https://picsum.photos/g/620/349" alt="blah"/>
+                    <h3>JLR</h3>
+                    <p>Some text description goes here</p>
+                </div>
+                <div class="btp">
+                <img src="https://picsum.photos/620/349?image=23" alt="blah"/>
+                    <h3>British Transport Police</h3>
+                    <p>Some text description goes here</p>
+                </div>
             </div>
         )
     }
