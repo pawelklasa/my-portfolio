301f462874755857227e48724cdde2683005a61b
Pawel Klasa <pawel.klasa@gmail.com>
2018-02-07 14:25:55 +0000

    added 4 items on work page

--- src/components/pages/work.js
+++ src/components/pages/work.js
@@ -9,13 +9,27 @@ import './styles/work.css';
 export class Work extends React.Component {
     render() {
         return (
-            <div class="">
+            <div class="content content-two">
                 <div class="talktalk">
-                    Some content
-               </div>
-               <div class="sky">
-                    Some content two
-               </div>
+                    <img src="https://picsum.photos/620/349?image=29" alt="blah"/>
+                    <h3>TalkTalk</h3>
+                    <p>Some text description goes here</p>
+                </div>
+                <div class="sky">
+                    <img src="https://picsum.photos/620/349?image=25" alt="blah"/>
+                    <h3>SKY</h3>
+                    <p>Some text description goes here</p>
+                </div>
+                <div class="jlr">
+                    <img src="https://picsum.photos/g/620/349" alt="blah"/>
+                    <h3>JLR</h3>
+                    <p>Some text description goes here</p>
+                </div>
+                <div class="btp">
+                <img src="https://picsum.photos/620/349?image=23" alt="blah"/>
+                    <h3>British Transport Police</h3>
+                    <p>Some text description goes here</p>
+                </div>
             </div>
         )
     }
