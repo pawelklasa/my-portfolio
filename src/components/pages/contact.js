9d2b4fd78825ad9aa12f25eb4c714b573af46180
Pawel Klasa <pawel.klasa@gmail.com>
2018-02-28 10:36:16 +0000

    change text

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -9,7 +9,7 @@ export class Contact extends React.Component {
         return (
             <div class="">
                 <div>
-                    Contact
+                    Contact me
                </div>
             </div>
         )
