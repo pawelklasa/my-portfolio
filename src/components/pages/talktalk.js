5b04b4dc0655492e3538316b0657f6fdd9f2daa4
Pawel Klasa <pawel.klasa@gmail.com>
2018-04-12 11:15:10 +0100

    remove tet

--- src/components/pages/talktalk.js
+++ src/components/pages/talktalk.js
@@ -11,7 +11,7 @@ export class Talktalk extends React.Component {
         return (
             <div class="content">
                 <h2 class="title">TalkTalk</h2>
-                <p class="about">I've spent five years at TalkTalk. First working as a Digital Designer. Then working as a front-end developer looking after TalkTalk's single page applications. </p>
+                <p class="about">I've spent five years at TalkTalk. First working as a Digital Designer.</p>
             </div>
 
         )
