a13e34f21b212853de21e87187d090794bce7dc6
Pawel Klasa <pawel.klasa@gmail.com>
2018-04-12 11:15:58 +0100

    description

--- src/components/pages/talktalk.js
+++ src/components/pages/talktalk.js
@@ -11,7 +11,7 @@ export class Talktalk extends React.Component {
         return (
             <div class="content">
                 <h2 class="title">TalkTalk</h2>
-                <p class="about">Some text goes here...</p>
+                <p class="about">Some text/description goes here...</p>
             </div>
 
         )
